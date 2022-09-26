647ea1c0c084d21622ac9d6e8d09baf118ef4868
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-09-26 08:59:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/landscapevslayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.landscapevslayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
